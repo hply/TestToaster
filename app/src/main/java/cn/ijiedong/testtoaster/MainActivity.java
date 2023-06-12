8821bfeb464613db6f460c8dc25f2dd2b8ad2cd0
jiedong.wang <jiedong.wang>
2023-06-12 09:39:29 +0800

    添加多跳转测试

--- app/src/main/java/cn/ijiedong/testtoaster/MainActivity.java
+++ app/src/main/java/cn/ijiedong/testtoaster/MainActivity.java
@@ -1,17 +1,22 @@
 package cn.ijiedong.testtoaster;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.view.KeyEvent;
 import android.view.View;
 
 import com.hjq.toast.Toaster;
 
 public class MainActivity extends AppCompatActivity {
+
+    private long lastClickTime = 0L;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,14 +25,55 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void toBackgroundShowToast(View v) {
-        go2AppDetailSettings();
-        Toaster.delayedShow("请打开存储相机xxxxx权限",200);
-    }
-
-    public void go2AppDetailSettings() {
+        Toaster.showLong("请打开存储相机xxxxx权限");
         Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         intent.setData(Uri.fromParts("package", getPackageName(), null));
-        this.startActivity(intent);
+        startActivity(intent);
+    }
+
+    public void toDesktopToast(View v) {
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        startActivity(intent);
+        Toaster.showLong("请打开存储相机xxxxx权限");
+    }
+
+    public void clickActionSend(View view) {
+        Toaster.showLong("请打开存储相机xxxxx权限");
+        Intent shareIntent = new Intent();
+        shareIntent.setAction(Intent.ACTION_SEND);
+        shareIntent.putExtra(Intent.EXTRA_TEXT, "toaster使用demo");
+        shareIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        shareIntent.setType("text/plain");
+        startActivity(Intent.createChooser(shareIntent, "请选择打开的app"));
+    }
+
+    public void clickLaunchQQ(View v) {
+        Toaster.showLong("请打开存储相机xxxxx权限");
+        PackageManager packageManager = this.getPackageManager();
+        Intent intent = packageManager.getLaunchIntentForPackage("com.tencent.mobileqq");
+        startActivity(intent);
+    }
+
+    public void clickLaunchWeChat(View v) {
+        Toaster.showLong("请打开存储相机xxxxx权限");
+        PackageManager packageManager = this.getPackageManager();
+        Intent intent = packageManager.getLaunchIntentForPackage("com.tencent.mm");
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            if (System.currentTimeMillis() - lastClickTime >= 2000) {
+                Toaster.showShort("再次点击退出");
+                lastClickTime = System.currentTimeMillis();
+            } else {
+                onBackPressed();
+            }
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
